d4357377efdb824f3dc62b9eb42644f21937afc3
Miguel Ramirez Ramirez Cortes <miguel.ricardo@Miguels-MacBook-Pro.local>
2023-09-23 12:26:23 -0500

    just pip3 install

--- lambda-layer.tf
+++ lambda-layer.tf
@@ -12,10 +12,6 @@ resource "null_resource" "lambda_layer" {
     # the command to install python and dependencies to the machine and zips
     provisioner "local-exec" {
         command = <<EOT
-            apt-get update
-            apt install python3 python3-pip zip -y
-            rm -rf python
-            mkdir python
             pip3 install -r ${local.requirements_path} -t python/
             zip -r ${local.layer_zip_path} python/
         EOT
