a5971c2820741c11f03128e0debe749da7f3a50e
Miguel Ramirez Ramirez Cortes <miguel.ricardo@Miguels-MacBook-Pro.local>
2023-09-23 07:15:27 -0500

    Improving lambda layer

--- lambda-layer.tf
+++ lambda-layer.tf
@@ -1,7 +1,7 @@
 locals {
   layer_zip_path    = "layer.zip"
   layer_name        = "my_lambda_requirements_layer"
-  requirements_path = "requirements.txt"
+  requirements_path = "${path.root}/../requirements.txt"
 }
 
 # create zip file from requirements.txt. Triggers only when the file is updated
@@ -12,7 +12,6 @@ resource "null_resource" "lambda_layer" {
     # the command to install python and dependencies to the machine and zips
     provisioner "local-exec" {
         command = <<EOT
-            set -e
             apt-get update
             apt install python3 python3-pip zip -y
             rm -rf python
