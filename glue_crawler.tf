6dcfa06f6e8b788e4e6299fdf924de487831aa31
Miguel Ramirez Ramirez Cortes <miguel.ricardo@enaats-bal-orioles-gw.net.disney.com>
2023-09-26 12:06:22 -0500

    Improve Glue Crawler IAM access

--- glue_crawler.tf
+++ glue_crawler.tf
@@ -1,27 +1,41 @@
 ###################################################
 #         IAM
 ###################################################
-resource "aws_iam_role" "glue_crawler_role" {
-  name = "glue-crawler-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "glue.amazonaws.com"
-        }
-      }
-    ]
-  })
+data "aws_iam_policy_document" "glue_crawler_policy_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["glue.amazonaws.com"]
+      type        = "Service"
+    }
+  }
 }
 
+data "aws_iam_policy_document" "glue_crawler_s3_policy_document" {
+  statement {
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = [ 
+        "arn:aws:s3:::${module.s3_bucket_landing_transformed.s3_bucket_id}/*",
+        "arn:aws:s3:::${module.s3_bucket_landing_transformed.s3_bucket_id}", 
+    ]
+  }
+}
 
-resource "aws_iam_role_policy_attachment" "glue_crawler_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
-  role       = aws_iam_role.glue_crawler_role.name
+resource "aws_iam_role" "glue_crawler_service_role" {
+  name               = "glue-crawler-service-role"
+  assume_role_policy = data.aws_iam_policy_document.glue_crawler_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "glue_crawler_service_role_policy_attachment" {
+    role       = aws_iam_role.glue_crawler_service_role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueCrawlerServiceRole"
+}
+
+resource "aws_iam_role_policy" "glue_crawler_service_role_policy" {
+  name   = "glue-crawler-service-role-policy"
+  policy = data.aws_iam_policy_document.glue_crawler_s3_policy_document.json
+  role   = aws_iam_role.glue_crawler_service_role.id
 }
 
 ###################################################
@@ -36,7 +50,7 @@ resource "aws_glue_catalog_database" "streaming_database" {
 resource "aws_glue_crawler" "streaming_crawler" {
   database_name   = aws_glue_catalog_database.streaming_database.name
   name            = var.glue_crawler.crawler_name
-  role            = aws_iam_role.glue_crawler_role.arn
+  role            = aws_iam_role.glue_crawler_service_role.arn
   s3_target {
     path = module.s3_bucket_landing_transformed.s3_bucket_id
   }
